7db2784270c38cc22d1cd6434ca073c9b5c5e334
Darren <darren.wu@groupkab.com>
2017-05-09 21:16:55 +0800

    EP5

---
+++ gameofchats/LoginController+handlers.swift
@@ -0,0 +1,9 @@
+//
+//  LoginController+handlers.swift
+//  gameofchats
+//
+//  Created by 吳得人 on 2017/5/9.
+//  Copyright © 2017年 吳得人. All rights reserved.
+//
+
+import Foundation
---
+++ gameofchats/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  gameofchats
+//
+//  Created by 吳得人 on 2017/5/9.
+//  Copyright © 2017年 吳得人. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
