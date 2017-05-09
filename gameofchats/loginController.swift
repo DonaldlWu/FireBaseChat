c7c488407a90cf1f12512674edcae27070e0a516
Darren <darren.wu@groupkab.com>
2017-05-09 22:43:40 +0800

    EEP5

--- gameofchats/loginController.swift
+++ gameofchats/loginController.swift
@@ -56,39 +56,6 @@ class loginController: UIViewController {
         })
     }
     
-    func handleRegister(){
-        guard let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text
-            else {
-            print("Form is not valid")
-            return
-        }
-        
-        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion:
-            { (user: FIRUser?, error) in
-        
-            if error != nil {
-                print(error!)
-                return
-            }
-                
-            guard let uid = user?.uid else {
-                return
-            }
-            // Successfully authenticated user and save data
-            
-            let ref = FIRDatabase.database().reference(fromURL: "https://gameofchats-28ae8.firebaseio.com/")
-                let userReference = ref.child("users").child(uid)
-                let value = ["name": name, "email": email]
-                userReference.updateChildValues(value, withCompletionBlock: { (err, ref)
-                    in
-                    if err != nil {
-                        print(err!)
-                    }
-                    self.dismiss(animated: true, completion: nil)
-                })
-        })
-    }
-
     let nameTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Name"
